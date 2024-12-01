e43c727c08868c88f42a9c985f1491ed0e585cf2
Abdullah Firihy <abdullahfirihy96@outlook.com>
2024-12-01 20:53:05 +0200

    fixing typos

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,15 +1,15 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg md:max-w-md lg:max-w-lg">
+    <div className="user-profile bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm mx-auto my-12 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto md:w-44 md:h-44"
+        className="rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36 mx-auto"
       />
-      <h1 className="text-xl text-blue-800 my-4 md:text-2xl lg:text-3xl">
+      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4 text-center">
         John Doe
       </h1>
-      <p className="text-gray-600 text-base md:text-lg lg:text-xl">
+      <p className="text-sm sm:text-base md:text-lg text-gray-600 text-center">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
